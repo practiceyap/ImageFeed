23db94b70890a9c23de08b411d52aecd6346c464
practiceyap <sashastronk4@gmail.com>
2023-09-27 00:35:40 +0600

    add functional in TabBarController

--- ImageFeed/TabBarController.swift
+++ ImageFeed/TabBarController.swift
@@ -8,23 +8,25 @@
 import UIKit
 
 final class TabBarController: UITabBarController {
-
     override func awakeFromNib() {
         super.awakeFromNib()
-
         let storyboard = UIStoryboard(name: "Main", bundle: .main)
-        let imagesListViewController = storyboard.instantiateViewController(withIdentifier: "ImagesListViewController")
-        imagesListViewController.tabBarItem = UITabBarItem(title: nil,
-                                                           image: UIImage(named: "tab_editorial_active"),
-                                                           selectedImage: nil)
+        let imagesListViewController = storyboard.instantiateViewController(
+            withIdentifier: "ImagesListViewController"
+        ) as? ImagesListViewController
+        guard let imagesListViewController else { return }
+        imagesListViewController.configure(ImagesListViewPresenter())
+        
         view.backgroundColor = .ypBlack
-
+        
         let profileViewController = ProfileViewController()
+        profileViewController.configure(ProfilePresenter())
         profileViewController.tabBarItem = UITabBarItem(
             title: nil,
             image: UIImage(named: "tab_profile_active"),
             selectedImage: nil
         )
+        
         self.viewControllers = [imagesListViewController, profileViewController]
     }
 }
